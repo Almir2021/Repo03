e57346dc3bda1cc0f19323f630ea405f88a60c52
Almir2021 <bosnjicalmir@gmail.com>
2021-09-15 18:24:26 +0200

    Dodajem vjezbu mnozenja

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+    //Only change code below this line 
+    var numOne;
+    var numTwo;
+    var product;
+    //multiplication
+    product = numOne * numTwo;
+
+    //Only change code below this line
+    return product;
+}
+console.log(main(15, 3));
+
+module.exports = main;
