a68aecb6d5fdfc7421d90d0f7c6cabca9f67743b
Almir2021 <bosnjicalmir@gmail.com>
2021-09-15 14:29:08 +0200

    Dodajem vjezbu 2

---
+++ ex02/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+    //Only change code below this line 
+
+    // This is a single line JavaScript comment
+
+    /*document.write("this is a multi-line comment");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+     */
+
+
+    //Only change code below this line 
+}
+console.log(main());
+
+module.exports = main;
