cbd6416e7306f15d568126007699d8479cd1ada3
Almir2021 <bosnjicalmir@gmail.com>
2021-09-15 15:11:50 +0200

    Dodajem vjezbu 4

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    //Only change code below this line 
+
+    // This is a single line JavaScript comment
+    var x = 10;
+    var y = x;
+
+    //Only change code below this line
+    return [x, y];
+}
+console.log(main());
+
+module.exports = main;
