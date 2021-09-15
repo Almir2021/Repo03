d0be8218a0e351efbb388c7ee556d9b342bd275b
Almir2021 <bosnjicalmir@gmail.com>
2021-09-15 17:51:16 +0200

    Predajem vjezbu 8

---
+++ ex08/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+    //Only change code below this line 
+    var numOne;
+    var numTwo;
+    var sum;
+    // Addition
+    sum = numOne + numTwo;
+
+    //Only change code below this line
+    return sum;
+}
+console.log(main(5, 28));
+
+module.exports = main;
