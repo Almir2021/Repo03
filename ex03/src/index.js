b7c6bdfc10fc3dbc8e73bb7ffa86fd3fa7146a44
Almir2021 <bosnjicalmir@gmail.com>
2021-09-15 14:53:14 +0200

    Zavrsio sam vjezbu 3

---
+++ ex03/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    //Only change code below this line 
+
+    // This is a single line JavaScript comment
+    var forCat;
+
+    //Only change code below this line 
+}
+console.log(main());
+
+module.exports = main;
